870890113a5f2273a99ffa7343fea499442a3f0e
FRANCO EXEQUIEL SCHULER ALLUB <franschuler13@gmail.com>
2022-11-06 21:49:43 +0100

    added auth

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use(auth);
 server.use(middlewares);
 server.use(router);
 
