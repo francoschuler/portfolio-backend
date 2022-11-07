beaa3f18704005b952cf8a635097a1179d6fc119
Franco Exequiel Schuler <fran.schuler@vipera.com>
2022-11-07 17:17:10 +0100

    configured router and cors

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = 3001;
 
+server.db = router.db;
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', `http://localhost:${port}`)
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+  })
 server.use(auth);
 server.use(middlewares);
 server.use(router);
+
 
 server.listen(port);
