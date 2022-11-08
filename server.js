0537af3f08f65eba804fc887d4d0ed698d4d130f
Franco Exequiel Schuler <fran.schuler@vipera.com>
2022-11-08 09:10:55 +0100

    port fixes

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 server.db = router.db;
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', `http://localhost:${port}`)
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
+    res.header('Access-Control-Allow-Origin', `http://localhost:4200`);
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
   })
 server.use(auth);
 server.use(middlewares);
